34683a71cfa47d94d954d711c148402d09b9c40d
veeeks <43359785+veeeks@users.noreply.github.com>
2021-01-09 13:06:23 +0530

    Update main.py
    Added additional features such as time, information about someone/something, playing songs on youtube and  telling a joke

--- main.py
+++ main.py
@@ -1,26 +1,63 @@
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+
+from pyjokes import get_joke
+
+
 listener = sr.Recognizer()
-engine = pyttsx3.init();
+engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice',voices[1].id)
+rate = engine.getProperty('rate')   # getting details of current speaking rate
+print (rate)                        #printing current voice rate
+engine.setProperty('rate', 150)     # setting up new voice rate
 
-def talk(name):
-    engine.say('Hi welcome back'+name)
+def talk(text):
+    engine.say(text)
     engine.runAndWait()
 
-try:
-    with sr.Microphone() as source:
-        print("listening...")
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        name = listener.recognize_google(voice)
-        command = command.lower()
-        if 'alexaW' in command:
-            engine.say(command)
-            engine.runAndWait()
-            talk(name)
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print("listening...")
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            name = listener.recognize_google(voice)
+            command = command.lower()
+            if 'venks' in command:
+                command=command.replace('venks', '')
+                talk(command)
+    except:
+        pass
+    return command
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song=command.replace('play','')
+        talk('playing'+song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk('Current time is ' + time)
+    elif 'who' in command:
+        info=command.replace('who is ','')
+        info=wikipedia.summary(info,1)
+        talk(info)
+    elif 'what ' in command:
+        info=command.replace('what is','')
+        info=wikipedia.summary(info,2)
+        talk('  '+ info)
+    elif 'joke' in command:
+        #joke=command.replace('tell me a joke','')
+        joke=get_joke()
+        talk('Here is a joke' + joke)
 
 
-except:
-    pass
+
+
+run_alexa()
